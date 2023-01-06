368bffabc3ea8a6cfec20ce0fe06146a3307aa90
kevinmichallik <kevin.michallik@web.de>
2023-01-06 12:45:55 +0100

    Projekt W3 Tag5

---
+++ Aufgabe2/src/Runden.kt
@@ -0,0 +1,11 @@
+import kotlin.math.*
+
+fun main() {
+    rundenUndAusgeben(123.45)
+}
+
+fun rundenUndAusgeben(zahl: Double) {
+
+    val gerundeteZahl = round(zahl)
+    println(gerundeteZahl)
+}
---
+++ Aufgabe3/src/AbsolutRechner.kt
@@ -0,0 +1,14 @@
+import kotlin.math.*
+
+fun main() {
+
+    println(absoluteZahl(20.0))
+
+}
+
+fun absoluteZahl(zahl:Double){
+
+    var abs = abs(zahl)
+    println(abs)
+
+}
